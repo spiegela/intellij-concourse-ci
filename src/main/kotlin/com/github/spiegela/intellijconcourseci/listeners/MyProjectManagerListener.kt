ba688cbff2edf9768f6f229eeb2791944c2ab751
GitHub Action <action@github.com>
2020-07-03 17:49:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/spiegela/intellijconcourseci/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/spiegela/intellijconcourseci/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.spiegela.intellijconcourseci.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.spiegela.intellijconcourseci.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/spiegela/intellijconcourseci/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.spiegela.intellijconcourseci.services
+
+import com.github.spiegela.intellijconcourseci.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
